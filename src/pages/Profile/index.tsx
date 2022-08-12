52e26cf944cef914c71406f2c36de90de44e495c
Eliezer Garbin <eliezergarbin1@gmail.com>
2022-08-11 23:39:31 -0300

    ProfileData Created

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Container, Main, LeftSide, RightSide } from './styles';
 
-// import { Container } from './styles';
+import ProfileData from '../../components/ProfileData';
 
 const Profile: React.FC = () => {
-    return <div />;
+    return (
+        <Container>
+            <Main>
+                <LeftSide>
+                    <ProfileData 
+                        username={'eliezergarbin'}
+                        name={'Eliezer Nascimento'}
+                        avatarUrl={'https://avatars.githubusercontent.com/u/59988262?v=4'}
+                        followers={18}
+                        following={16}
+                        company={'me'}
+                        location={'SP, Brazil'}
+                        email={'eliezergarbin1@gmail.com'}
+                        blog={'linkedin.com/in/eliezergarbin'}
+                    />
+
+                </LeftSide>
+                <RightSide></RightSide>
+            </Main>
+        </Container>
+    );
 }
 
 export default Profile;
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Main = styled.div``;
+
+export const LeftSide = styled.div``;
+
+export const RightSide = styled.div``;
