6ad24074093b512eea2efbe7e4969ea988f6b41a
Eliezer Garbin <eliezergarbin1@gmail.com>
2022-08-13 00:02:27 -0300

    estilizando botside

--- src/components/RepoCard/styles.ts
+++ src/components/RepoCard/styles.ts
@@ -3,40 +3,40 @@ import { RiBookMarkLine, RiStarLine } from "react-icons/ri";
 import { AiOutlineFork } from "react-icons/ai";
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 16px;
-    border: 1px solid var(--border);
-    border-radius: 6px;
-
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 16px;
+  border: 1px solid var(--border);
+  border-radius: 6px;
 `;
 
 export const Topside = styled.div`
-    > header {
-        display: flex;
-        align-items: center;
+  > header {
+    display: flex;
+    align-items: center;
 
-        > a {
-            margin-left: 8px;
-            font-size: 14px;
-            font-weight: 600;
-            color: var(--link);
+    > a {
+      margin-left: 8px;
+      font-size: 14px;
+      font-weight: 600;
+      color: var(--link);
 
-            text-decoration: none;
+      text-decoration: none;
 
-            &:focus, &:hover {
-                text-decoration: underline;
-            }
-        }
+      &:focus,
+      &:hover {
+        text-decoration: underline;
+      }
     }
+  }
 
-    > p {
-        margin: 8px 0 16px;
-        font-size: 12px;
-        color: var(--gray);
-        letter-spacing: 0.1px;
-        }
+  > p {
+    margin: 8px 0 16px;
+    font-size: 12px;
+    color: var(--gray);
+    letter-spacing: 0.1px;
+  }
 `;
 
 const iconCSS = css`
@@ -50,7 +50,37 @@ export const RepoIcon = styled(RiBookMarkLine)`
   ${iconCSS}
 `;
 
-export const Botside = styled.div``;
+export const Botside = styled.div`
+  > ul {
+    display: flex;
+    align-items: center;
+
+    > li {
+      display: flex;
+      align-items: center;
+      margin-right: 16px;
+
+      > span {
+        margin-left: 5px;
+        font-size: 12px;
+        color: var(--gray);
+      }
+    }
+  }
+
+  .language {
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    flex-shrink: 0;
+  } &.other {
+    background: var(--other-language);
+  }&.javascript {
+    background: var(--javascript);
+  }&.typescript {
+    background: var(--typescript);
+  }
+`;
 
 export const StarIcon = styled(RiStarLine)`
   ${iconCSS}
