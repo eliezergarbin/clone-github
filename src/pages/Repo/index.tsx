1080a36c46a1f36e238de4f3eb68a4b76bde1d4e
Eliezer Garbin <eliezergarbin1@gmail.com>
2022-08-14 17:12:14 -0300

    estilizando page Repo

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-    return <div />;
-}
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={'username'} to={'/eliezergarbin'}>
+          eliezergarbin
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/eliezergarbin/Ecoleta'}>
+          ecoleta
+        </Link>
+      </Breadcrumb>
+
+      <p>Ecoleta Project: instructed by Diego Fernandes CTO at Rocketseat.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/eliezergarbin/Ecoleta"}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -1,4 +1,90 @@
+import styled, { css } from "styled-components";
+import { RiBookMarkLine, RiStarLine } from "react-icons/ri";
+import { AiOutlineFork } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
 
-import styled from 'styled-components';
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
 
-export const Container = styled.div``;
+  > p {
+    font-size: 16px;
+  }
+`;
+
+export const Breadcrumb = styled.div`
+  margin-bottom: 16px;
+
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  white-space: nowrap;
+
+  font-size: 18px;
+
+  > a {
+    color: var(--link);
+    text-decoration: none;
+
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+
+    &.username {
+      margin-left: 8px;
+    }
+
+    &.reponame {
+      font-weight: 600;
+    }
+  }
+
+  > span {
+    padding: 0 5px;
+  }
+`;
+
+const iconCSS = css`
+  width: 16px;
+  height: 16px;
+  fill: var(--icon);
+  flex-shrink: 0;
+`;
+
+export const RepoIcon = styled(RiBookMarkLine)`
+  ${iconCSS}
+`;
+
+export const Stats = styled.ul`
+ margin-top: 16px;
+ display: flex;
+ align-items: center;
+
+ > li {
+    display: flex;
+    align-items: center;
+    margin-right: 9px;
+
+    > * {
+        margin-right: 7px;
+        color: var(--gray);
+    }
+ }
+
+`;
+
+export const StarIcon = styled(RiStarLine)`
+  ${iconCSS}
+`;
+
+export const ForkIcon = styled(AiOutlineFork)`
+  ${iconCSS}
+`;
+
+export const LinkButton = styled.a``;
+
+export const GithubIcon = styled(FaGithub)`
+  ${iconCSS}
+`;
